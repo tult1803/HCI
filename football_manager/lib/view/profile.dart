02b642c9aa205fa82872057a894d04cd185b183b
DatVTP <vanthanhphuongdat1309@gmai.com>
2020-10-02 13:57:17 +0700

    Create Profile Page

--- football_manager/lib/view/profile.dart
+++ football_manager/lib/view/profile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:football_manager/url/url.dart';
 
@@ -7,6 +8,164 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+
+  Widget _getInfo() {
+    return Positioned(
+      top: 25,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        height: 260,
+        width: MediaQuery.of(context).size.width * 0.90,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text("User information:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60,
+                    child: Image.asset(''),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("SS_Flutter"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("Dev"),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Email:"),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text("ssflutterdev@gmail.com")
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _userAdress() {
+    return Positioned(
+      top: 300,
+      child: Container(
+        margin: EdgeInsets.all(20),
+        height: 200,
+        width: MediaQuery.of(context).size.width * 0.90,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "User Address:",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Village:"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Nongnioa village"),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("District:"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Sikhottabong district"),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Province:"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Vietiane capital"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _btnLogout() {
+    return Positioned(
+      top: 435,
+      child: Container(
+        margin: EdgeInsets.all(100),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 1.4 * (MediaQuery.of(context).size.height / 20),
+              width: 5 * (MediaQuery.of(context).size.width / 10),
+              margin: EdgeInsets.only(bottom: 20),
+              child: RaisedButton(
+                elevation: 5.0,
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Logout",
+                  style: TextStyle(
+                    color: Colors.green,
+                    letterSpacing: 1.5,
+                    fontSize: MediaQuery.of(context).size.height / 40,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,9 +174,19 @@ class _ProfilePageState extends State<ProfilePage> {
               fit: BoxFit.fill,
               image: AssetImage(url_imgbackground_welcome))
       ),
-//      child: SafeArea(
-//
-//      ),
+     child: SafeArea(
+       child: Stack(
+         children: <Widget>[
+           Container(
+             height: MediaQuery.of(context).size.height,
+             width: MediaQuery.of(context).size.width,
+           ),
+           _getInfo(),
+           _userAdress(),
+           _btnLogout(),
+         ],
+       ),
+     ),
     );
   }
 }
